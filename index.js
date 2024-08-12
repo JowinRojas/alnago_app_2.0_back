7c74ed49785d8da7acbf6e7afb3cec36a6514184
Schwaaprogramador <danieltoro.programacion@gmail.com>
2024-08-12 09:00:51 -0500

    configuracion inicial

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-import express from 'express';
 
-const app = express()
+import app from "./src/app.js";
 
-app.listen(3000, ()=>{
-    console.log('sever on')
-})
+const { PORT } = process.env;
+// const mongoose = require('mongoose');
+require("dotenv").config();
+
+const main = async () => {
+    try {
+        //await mongoose.connect(DATA_BASE_LINK);
+        app.listen(PORT, () => {
+            console.log("conectado en el puerto" + PORT)
+          })
+        
+    } catch (error) {
+        console.log("no se pudo conectar pa", error)
+    }
+}
+
+main();
---
+++ src/app.js
@@ -0,0 +1,26 @@
+
+import express from 'express'
+
+export const server = express();
+require("dotenv").config();
+
+// const morgan = require('morgan');
+// const cors = require('cors');
+// const cookieParser = require('cookie-parser');
+//const bcrypt = require('bcryptjs');
+
+//MIDDLEWARES - de henry
+// server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+// server.use(bodyParser.json({ limit: '50mb' }));
+
+
+//MIDDLEWARES - DE FASTZODE
+// server.use(morgan('dev'));
+// server.use(express.json());
+// server.use(cookieParser());
+
+// //CORSs
+// server.use(cors({
+//     credentials:true,
+//     origin:'http://localhost:4321',
+// }));
