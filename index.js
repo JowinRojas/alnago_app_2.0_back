d20550db28cdf68bebd49373690b5b71d725a083
Schwaaprogramador <danieltoro.programacion@gmail.com>
2024-08-12 09:28:10 -0500

    configuracion inicial

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 
-import app from "./src/app.js";
-
+import {server} from "./src/app.js";
+import dotenv from 'dotenv';
+dotenv.config();
 const { PORT } = process.env;
+
 // const mongoose = require('mongoose');
-require("dotenv").config();
+
 
 const main = async () => {
     try {
         //await mongoose.connect(DATA_BASE_LINK);
-        app.listen(PORT, () => {
-            console.log("conectado en el puerto" + PORT)
+        server.listen(PORT, () => {
+            console.log("conectado en el puerto " + PORT)
           })
         
     } catch (error) {
